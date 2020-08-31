6c46cd5c83f46b4df36e9bf1121a9780c6a28265
Alexander Hopgood <alex.hopgood@gmail.com>
2020-08-31 16:54:27 +0100

    Extending line length to 120 to match intellij's.

--- src/main/java/com/alexander/diagrams/db/MySQLRegexParser.java
+++ src/main/java/com/alexander/diagrams/db/MySQLRegexParser.java
@@ -25,8 +25,10 @@ public class MySQLRegexParser implements DatabaseSyntaxParser {
         }
         return Table.builder().name(tableName).build();
     }
-//    private static final String ATTRIBUTES_REGEX = "((?<notnull> NOT NULL)?(?<autoincrement> AUTO_INCREMENT)?( DEFAULT (?<default>NULL|'[0-9]')?))";
-    private static final String ATTRIBUTES_REGEX = "((?<notnull> NOT NULL)?(?<autoincrement> AUTO_INCREMENT)?( DEFAULT (?<default>NULL|'[0-9]'))?)";
+//    private static final String ATTRIBUTES_REGEX =
+//      "((?<notnull> NOT NULL)?(?<autoincrement> AUTO_INCREMENT)?( DEFAULT (?<default>NULL|'[0-9]')?))";
+    private static final String ATTRIBUTES_REGEX =
+        "((?<notnull> NOT NULL)?(?<autoincrement> AUTO_INCREMENT)?( DEFAULT (?<default>NULL|'[0-9]'))?)";
     private static final String AUTO_INCREMENT_GROUP = "autoincrement";
     private static final String NOT_NULL_GROUP = "notnull";
     private static final String DEFAULT_GROUP = "default";
@@ -35,13 +37,16 @@ public class MySQLRegexParser implements DatabaseSyntaxParser {
     // int scale maximum int (10)
     // varchar scale maximum (65,535) - using [0-9]{0,5} for brevity allowing numbers from 0 to 99,999
     // see {@link https://dev.mysql.com/doc/refman/8.0/en/char.html}
-    private static final String NUMBER_SCALE_REGEX = "(\\((?<scale>([0-9]{0,5})|([0-9]|[1-5][0-9]|[6][1-5]),([0-9]|[1-2][0-9]|[3][0]))\\))?";
+    private static final String NUMBER_SCALE_REGEX =
+        "(\\((?<scale>([0-9]{0,5})|([0-9]|[1-5][0-9]|[6][1-5]),([0-9]|[1-2][0-9]|[3][0]))\\))?";
     private static final String SCALE_GROUP = "scale";
 
-    private static final String DATATYPE_REGEX = "(?<datatype>int|varchar|tinyint|datetime|decimal|date)" + NUMBER_SCALE_REGEX;
+    private static final String DATATYPE_REGEX =
+        "(?<datatype>int|varchar|tinyint|datetime|decimal|date)" + NUMBER_SCALE_REGEX;
     private static final String DATATYPE_GROUP = "datatype";
 
-    private static final String COLUMN_REGEX = "\\s*`(?<columnName>[a-zA-Z-0-9\\-_]+)` " + DATATYPE_REGEX + ATTRIBUTES_REGEX + ",";
+    private static final String COLUMN_REGEX =
+        "\\s*`(?<columnName>[a-zA-Z-0-9\\-_]+)` " + DATATYPE_REGEX + ATTRIBUTES_REGEX + ",";
     private static final String COLUMN_NAME_GROUP = "columnName";
 
     private static final Pattern COLUMN_PATTERN = Pattern.compile(COLUMN_REGEX);
@@ -63,8 +68,9 @@ public class MySQLRegexParser implements DatabaseSyntaxParser {
     }
 
     private static final String FOREIGN_KEY_REGEX  = "\\s*CONSTRAINT `" + MYSQL_NAME_REGEX +
-            "` FOREIGN KEY \\(`(?<foreignKeyName>" + MYSQL_NAME_REGEX + ")`\\) REFERENCES `(?<sourceTable>" + MYSQL_NAME_REGEX + ")` " +
-            "\\(`(?<sourceColumn>" + MYSQL_NAME_REGEX + ")`\\).*";
+        "` FOREIGN KEY \\(`(?<foreignKeyName>" + MYSQL_NAME_REGEX + ")`\\) " +
+        "REFERENCES " +
+        "`(?<sourceTable>" + MYSQL_NAME_REGEX + ")` \\(`(?<sourceColumn>" + MYSQL_NAME_REGEX + ")`\\).*";
     private static final Pattern FOREIGN_KEY_PATTERN = Pattern.compile(FOREIGN_KEY_REGEX);
 
     private static final String FOREIGN_KEY_NAME_GROUP = "foreignKeyName";
